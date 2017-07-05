2b509fd1a7fdf41cd97e6708a3f67057b7537ba9
Luis Pallares <thepanchi@gmail.com>
2017-07-05 11:57:19 +0200

    add ES_HOST env var

--- enviroment_variables.sample.sh
+++ enviroment_variables.sample.sh
@@ -2,6 +2,7 @@
 
 export GDELT_HISTORIC_FILE_PATH='/Users/panchicore/www/es-gdelt/data/old/*.CSV'
 export GDELT_REALTIME_FILE_PATH='/Users/panchicore/www/es-gdelt/data/realtime/*.CSV'
+export ES_HOST=http://localhost:9200/
 export ES_USER=elastic
 export ES_PASSWORD=changeme
 export ES_GDELT_INDEX=gdelt
